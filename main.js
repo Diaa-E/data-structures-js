913394304d343f9671be94288302f43471dd1cce
Diaa-E <blarb21@yahoo.com>
2023-03-28 01:23:24 +0200

    Create node factory

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
 "use strict";
 
+function linkedListNode()
+{
+    let value = null;
+    let next = null;
+
+    const getValue = () => {
+
+        return value;
+    };
+
+    const setValue = (newValue) => {
+
+        value = newValue;
+    };
+
+    const setNext = (newNext) => {
+
+        next = newNext;
+    };
+
+    const getNext = () => {
+
+        return next;
+    };
+
+    return {getValue, getNext, setNext, setValue};
+}
