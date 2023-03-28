ab201c1a76158421f80fef21d38110aa4a92f96f
Diaa-E <blarb21@yahoo.com>
2023-03-28 22:17:49 +0200

    Create balanced tree factory

--- src/main.js
+++ src/main.js
@@ -40,3 +40,32 @@ function treeNode()
 
     return {getRight, setRight, setLeft, getLeft, setData, getData};
 }
+
+function balancedTree(array)
+{
+    array.sort();
+    array = [...new Set(array)]; //remove duplicates
+    
+    let root = buildTree(array, 0, array.length - 1);
+
+    const buildTree = (sortedArray, start, end) => {
+
+        //base vase
+        if (start > end) return null;
+
+        let mid = parseInt((start + end) / 2);
+
+        let node = treeNode();
+        node.setLeft(buildTree(sortedArray, start, mid - 1));
+        node.setRight(buildTree(sortedArray, mid + 1, end));
+
+        return node;
+    }
+
+    const getRoot = () => {
+
+        return root.getData();
+    }
+
+    return {getRoot}
+}
