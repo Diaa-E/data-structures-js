1f8629dd547f7bd4053eed366b168b82dc2fef17
Diaa-E <blarb21@yahoo.com>
2023-03-31 02:31:13 +0200

    Use a callback instead of data array

--- src/binaryTree.js
+++ src/binaryTree.js
@@ -177,19 +177,26 @@ export function balancedTree(array)
         const data = [];
         q.push(root)
 
-        while (q.length !== 0)
+        while (q.length > 0)
         {
             let cursor = q[0];
-            data.push(cursor.data);
+            
+            if (typeof callback === 'function') 
+            {
+                callback(q[0]);
+            }
+            else
+            {
+                data.push(cursor.data);
+            }
 
-            if (typeof callback === 'function') callback(q[0]);
             if (cursor.left !== null) q.push(cursor.left);
             if (cursor.right !== null) q.push(cursor.right);
 
             q.shift();
         }
 
-        return typeof callback === 'function'? null : data //check if a function is passed as an argument
+        return typeof callback === 'function'? undefined : data //check if a function is passed as an argument
     }
 
     //copied from The Odin Project, refitted for my code
