7b7ff893555ae4147f5beacdf131f91834877b94
Diaa-E <blarb21@yahoo.com>
2023-04-13 04:23:01 +0200

    Create test for graph

--- src/main.js
+++ src/main.js
@@ -2,17 +2,43 @@
 
 import { linkedList } from "./linkedList";
 import { balancedTree } from "./binaryTree";
+import { listGraph } from "./graph";
 import { getRandomArray, getRandomNumber } from "./utility";
 
-let tree = balancedTree(getRandomArray(500, 10))
-tree.prettyPrint()
+// let tree = balancedTree(getRandomArray(500, 10))
+// tree.prettyPrint()
 
-for (let i = 0; i <= 10 ; i++)
-{
-    tree.insert(getRandomNumber(100))
-}
-tree.prettyPrint()
-console.log(tree.isBalanced())
+// for (let i = 0; i <= 10 ; i++)
+// {
+//     tree.insert(getRandomNumber(100))
+// }
+// tree.prettyPrint()
+// console.log(tree.isBalanced())
 
-tree.rebalance()
-tree.prettyPrint()
+// tree.rebalance()
+// tree.prettyPrint()
+
+const nodes = [15, 23, 24, 99, 10]
+const edges = [
+    [15, 23],
+    [24, 23],
+    [15, 24],
+    [99, 24],
+    [10, 24],
+    [10, 99],
+] 
+
+const graph = listGraph();
+
+nodes.forEach(node => {
+    
+    graph.addNode(node);
+});
+
+edges.forEach( edge => {
+
+    graph.addEdge(...edge)
+})
+
+console.log(graph.graph);
+graph.bfs(10, 23)
