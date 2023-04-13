feadbe114ebaf9519eac7c200f096163a93523c0
Diaa-E <blarb21@yahoo.com>
2023-04-13 04:21:37 +0200

    Search the graph breadth first

--- src/graph.js
+++ src/graph.js
@@ -16,5 +16,41 @@ export function listGraph()
         graph.get(destination).push(node);
     };
 
-    return {graph: graph, addNode, addEdge};
+    
+    function bfs(start, target)
+    {
+        if (graph.keys().length = 0) return; //empty graph
+
+        const q = [start];
+        const visited = new Set();
+        visited.add(start);
+
+        while (q.length > 0)
+        {
+            const currentNode = q.shift();
+            const currentEdges = graph.get(currentNode);
+
+            for (const edge of currentEdges) {
+
+                if (edge === target)
+                {
+                    console.log(`found ${target}`)
+                }
+                
+                if (!visited.has(edge))
+                {
+                    visited.add(edge);
+                    q.push(edge);
+                    console.log(edge);
+                }
+
+            }
+
+            console.log("--------")
+
+        }
+
+    }
+
+    return {graph: graph, addNode, addEdge, bfs};
 }
